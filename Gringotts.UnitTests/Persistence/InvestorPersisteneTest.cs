e4d66c9a77baa5e53917e0a09f46eb8d74106b89
Simanta <simantjh@.corporate.thoughtworks.com>
2010-03-30 14:19:33 +0530

    Added InvestorPersistenceTest

---
+++ Gringotts.UnitTests/Persistence/InvestorPersisteneTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Gringotts.Persistence
+{
+	[TestFixture]
+	public class InvsetorPersistenceTest
+	{
+		[Test]
+		public void METHOD_NAME()
+		{
+			Assert.AreEqual("expected", "actual");
+		}
+	}
+}
