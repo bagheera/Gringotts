b5e6dec3c2c0b2f69e7d607dae4339509a4a5772
unknown <akshayn@.corporate.thoughtworks.com>
2010-03-31 14:08:15 +0530

    [Akshay/Niranjan] Changes to Venture Test Cases

--- Gringotts.UnitTests/Domain/VentureTest.cs
+++ Gringotts.UnitTests/Domain/VentureTest.cs
@@ -14,30 +14,21 @@ namespace Gringotts.UnitTests.Domain
             Amount outlay = new Amount(100);
             Amount minInvestment = new Amount(1);
             Venture venture = new Venture(nameOfVenture, outlay, minInvestment);
-            Assert.IsNull(venture.Id);
             Assert.AreEqual(nameOfVenture, venture.Name);
             Assert.AreEqual(outlay, venture.Outlay);
             Assert.AreEqual(minInvestment, venture.MinInvestment);
-            Assert.Greater(venture.MinInvestment.Value, 0);
-            Assert.Greater(venture.Outlay.Value, venture.MinInvestment.Value);
         }
 
         [Test]
         public void Should_not_be_able_to_create_a_venture_for_negative_min_investment()
         {
-            Name nameOfVenture = new Name("Ventura");
-            Amount outlay = new Amount(100);
-            Amount minInvestment = new Amount(-1);
-            Assert.Throws<Exception>(delegate { new Venture(nameOfVenture, outlay, minInvestment); });            
+            Assert.Throws<Exception>(delegate { new Venture(new Name("Ventura"), new Amount(100), new Amount(-1)); });            
         }
 
         [Test]
         public void Should_not_be_able_to_create_a_venture_for_outlay_lesser_than_min_investment()
         {
-            Name nameOfVenture = new Name("Ventura");
-            Amount outlay = new Amount(0);
-            Amount minInvestment = new Amount(1);
-            Assert.Throws<Exception>(delegate { new Venture(nameOfVenture, outlay, minInvestment); });            
+            Assert.Throws<Exception>(delegate { new Venture(new Name("Ventura"), new Amount(0), new Amount(1)); });            
         }
     }
 }
--- Gringotts/Domain/Venture.cs
+++ Gringotts/Domain/Venture.cs
@@ -29,5 +29,32 @@ namespace Gringotts.Domain
             investor.Pay(amount);
             return new Investment();
         }
+
+        public virtual bool Equals(Venture other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.Id, Id) && Equals(other.Name, Name) && Equals(other.Outlay, Outlay) && Equals(other.MinInvestment, MinInvestment);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (Venture)) return false;
+            return Equals((Venture) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int result = (Id != null ? Id.GetHashCode() : 0);
+                result = (result*397) ^ (Name != null ? Name.GetHashCode() : 0);
+                result = (result*397) ^ (Outlay != null ? Outlay.GetHashCode() : 0);
+                result = (result*397) ^ (MinInvestment != null ? MinInvestment.GetHashCode() : 0);
+                return result;
+            }
+        }
     }
 }
