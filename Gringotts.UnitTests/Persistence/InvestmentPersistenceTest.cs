aedccf9c4ba665a8dd080a42ece74bede1282ae0
Akshay <akshay@gmail.com>
2010-04-01 18:08:42 +0530

    [Akshay/Sishir] hbm for Investment (incomplete)

---
+++ Gringotts.UnitTests/Persistence/InvestmentPersistenceTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NHibernate;
+using NUnit.Framework;
+using Gringotts.Domain;
+
+namespace Gringotts.Persistence
+{
+    [TestFixture]
+    public class InvestmentPersistenceTest : NHibernateInMemoryTestFixtureBase
+    {
+        private ISession session;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            InitalizeSessionFactory();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            session = CreateSession();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            session.Dispose();
+        }
+
+        [Test]
+        public void Should_Persist()
+        {
+            Investor investor = new Investor(new Name("Investor 1"), new GringottsDate(DateTime.Today), new Amount(100));
+            InvestorRepository investorRepository = new InvestorRepository();
+            investorRepository.Session = session;
+            investorRepository.Save(investor);
+
+            Venture venture = new Venture(new Name("Ventura"), new Amount(100), new Amount(1));
+            VentureRepository ventureRepository = new VentureRepository(session);
+            ventureRepository.Save(venture);    
+  
+            Investment investment = new Investment(investor, venture, new Amount(10));
+            InvestmentRepository investmentRepository = new InvestmentRepository(session);
+            investmentRepository.Save(investment);
+        }
+    }
+}
--- Gringotts/Domain/Investment.cs
+++ Gringotts/Domain/Investment.cs
@@ -5,21 +5,38 @@ namespace Gringotts.Domain
     public class Investment
     {        
         private readonly Investor investor;
-        
-        public Investment(Investor investor  , Amount amount)
+        private readonly Venture venture;
+        private string id;
+
+        public virtual string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public Investment(Investor investor, Amount amount) : this (investor, null, amount)
+        {
+        }
+
+        public Investment(Investor investor, Venture venture, Amount amount)
         {
             this.investor = investor;
             this.Value = amount;
+            this.venture = venture;
         }
 
-        public Amount Value { get; set; }
+        public Investment() {}
 
-        public bool HasInvestor(Investor investor)
+        public virtual Amount Value { get; set; }
+
+        public virtual bool HasInvestor(Investor investor)
         {
             return this.investor.Equals(investor);
         }
 
-        public void CreditSurplus(Amount creditSurplus)
+        public virtual void CreditSurplus(Amount creditSurplus)
         {
             investor.AcceptSurplus(creditSurplus);
         }
