53e0d31a5c77663970851c974bb2f6ce92d689f7
Ram <rc.chandru@gmail.com>
2010-05-04 16:13:46 +0530

    Mutiple Offers, Ventures and Investors

--- Gringotts.UnitTests/Persistence/OfferPersistenceTest.cs
+++ Gringotts.UnitTests/Persistence/OfferPersistenceTest.cs
@@ -29,12 +29,12 @@ namespace Gringotts.Persistence{
 
         [Test]
         public void ShouldFetchInvestorAndVentureForOffer(){
-            Investor investor = CreateInvestor();
+            Investor investor = CreateInvestor("Jagan", 4000);
             var investorRepository = new InvestorRepository(session);
 
             investorRepository.Save(investor);
 
-            Venture venture = CreateVenture();
+            Venture venture = CreateVenture(2000, 400, "Ram Capitalists");
             int minInvestment;
             int outlay;
             string ventureName;
@@ -68,20 +68,17 @@ namespace Gringotts.Persistence{
             Assert.AreEqual(venture.MinInvestment, savedOffer.Venture.MinInvestment);
         }
 
-        private Venture CreateVenture(){
-            const int outlay = 2000;
-            const int minInvestment = 400;
-            const string ventureName = "Ram Capitalists";
-            return new Venture(new Name(ventureName), new Amount(outlay), new Amount(minInvestment));
+        private Venture CreateVenture(int outlay, int minInvestment, string name){
+            return new Venture(new Name(name), new Amount(outlay), new Amount(minInvestment));
         }
 
         [Test]
         public void ShouldFetchOffersForInvestorAndVenture(){
-            Investor investor = CreateInvestor();
+            Investor investor = CreateInvestor("Jagan", 4000);
             InvestorRepository investorRepository = new InvestorRepository(session);
             investorRepository.Save(investor);
 
-            Venture venture = CreateVenture();
+            Venture venture = CreateVenture(2000, 400, "Ram Capitalists");
             VentureRepository ventureRepository = new VentureRepository(session);
             ventureRepository.Save(venture);
 
@@ -102,11 +99,66 @@ namespace Gringotts.Persistence{
             Assert.AreEqual(amount, fetchedVenture.SubscribedAmount());
         }
 
+        [Test]
+        public void ShouldFetchMultipleOffersForInvestor(){
+            Investor investor = CreateInvestor("Jagan1", 5000);
+            InvestorRepository investorRepository = new InvestorRepository(session);
+            investorRepository.Save(investor);
 
-        private Investor CreateInvestor(){
-            const int corpus = 4000;
-            const string investorName = "Jagan";
-            return new Investor(new Name(investorName), new Amount(corpus));
+            Venture venture1 = CreateVenture(3000, 500, "Ram1");
+            Venture venture2 = CreateVenture(6000, 1000, "Ram2");
+            VentureRepository ventureRepository = new VentureRepository(session);
+            ventureRepository.Save(venture1);
+            ventureRepository.Save(venture2);
+
+            Amount amount1 = new Amount(700);
+            Amount amount2 = new Amount(800);
+
+            Offer offer1 = new Offer(investor, amount1, venture1);
+            Offer offer2 = new Offer(investor, amount2, venture2);
+            OfferRepository offerRepository = new OfferRepository(session);
+            offerRepository.Save(offer1);
+            offerRepository.Save(offer2);
+
+            session.Flush();
+            session.Clear();
+
+            Investor fetchedInvestor = investorRepository.GetInvestorById(investor.Id);
+            Assert.AreEqual(new Amount(1500), fetchedInvestor.OfferValue);
+        }
+
+        [Test]
+        public void ShouldFetchMultipleSubscriptionsForVenture(){
+            Investor investor1 = CreateInvestor("Joy", 9000);
+            Investor investor2 = CreateInvestor("Roy", 6000);
+            InvestorRepository investorRepository = new InvestorRepository(session);
+            investorRepository.Save(investor1);
+            investorRepository.Save(investor2);
+
+            Venture venture = CreateVenture(8000, 1920, "Ace Ventura");
+            VentureRepository ventureRepository = new VentureRepository(session);
+            ventureRepository.Save(venture);
+
+            Amount amount1 = new Amount(712);
+            Amount amount2 = new Amount(423);
+
+            Offer offer1 = new Offer(investor1, amount1, venture);
+            Offer offer2 = new Offer(investor2, amount2, venture);
+            OfferRepository offerRepository = new OfferRepository(session);
+            offerRepository.Save(offer1);
+            offerRepository.Save(offer2);
+
+            session.Flush();
+            session.Clear();
+
+            Venture fetchedVenture = ventureRepository.GetVentureById(venture.Id);
+            Assert.AreEqual(new Amount(1135), fetchedVenture.SubscribedAmount());
+          
+            
+        }
+        
+        private Investor CreateInvestor(string name, int corpus){
+            return new Investor(new Name(name), new Amount(corpus));
         }
 
     }
