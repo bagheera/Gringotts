674c4345024a4164f5204d676a32e1cbd4a4604f
unknown <darshanj@.corporate.thoughtworks.com>
2010-05-05 16:04:08 +0530

    Fixed build

---
+++ Gringotts.UnitTests/Domain/TermsOfSplitTest.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+using NUnit.Framework;
+
+namespace Gringotts.Domain
+{
+    [TestFixture]
+    public class TestPercentage
+    {
+        [Test]
+        public void ShouldThrowExceptionWhenPercentageRatioIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => new Percentage(12.0f));
+            Assert.Throws<ArgumentException>(() => new Percentage(-12.0f));
+        }
+
+        [Test]
+        public void ShouldBeAbleToApplyPercentage()
+        {
+            var percentage = new Percentage(0.4f);
+            Assert.AreEqual(new Amount(40.0f), percentage.Apply(new Amount(100)));
+        }
+
+        [Test]
+        public void ShouldBeAbleToApplyRemainingPercentage()
+        {
+            var percentage = new Percentage(0.4f);
+            Assert.AreEqual(new Amount(60.0f), percentage.ApplyRemaining(new Amount(100)));
+        }
+    }
+}
